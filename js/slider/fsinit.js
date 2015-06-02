b510de74755588b8f96f61a4a81f8d3076008cc9
vitaliyolnk <githubvol@gmail.com>
2015-06-02 21:26:43 +0100

    master

---
+++ js/slider/fsinit.js
@@ -0,0 +1,17 @@
+﻿// Can also be used with $(document).ready()
+$(window).load(function() {
+  $('#flexcontainer').flexslider({
+    animation: "slide",
+    controlsContainer: '.flex-container'
+  });
+});
+
+$(window).load(function() {
+  $('#flexcontainer_carusel').flexslider(
+  {
+  	animation: "slide",
+    animationLoop: false,
+    itemWidth: 290,
+    itemMargin: 50
+  });
+});
